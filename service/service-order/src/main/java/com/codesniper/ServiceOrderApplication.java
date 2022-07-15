518ce481d91f4c837b6908863992175acab69fcd
CodeSn1per <codesniperrrrr@gmail.com>
2022-07-16 02:17:45 +0800

    搭建订单服务

---
+++ service/service-order/src/main/java/com/codesniper/ServiceOrderApplication.java
@@ -0,0 +1,13 @@
+package com.codesniper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceOrderApplication.class, args);
+    }
+
+}
